a2da6f87ac8b7760edf29a4be4fcc66d0270291e
yeGenieee <cyj95428@gmail.com>
2019-01-16 18:31:01 +0900

    기본 알고리즘_세 수의 최대값

---
+++ Algorithm/src/ch01/Max3.java
@@ -0,0 +1,27 @@
+package ch01;
+import java.util.Scanner;
+
+public class Max3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("세 정수의 최대값 구하기");
+        System.out.print("a의 값 : ");
+        int a = sc.nextInt();
+        System.out.print("b의 값 : ");
+        int b = sc.nextInt();
+        System.out.print("c의 값 : ");
+        int c = sc.nextInt();
+
+        int max = a; // 세 정수의 최대값 max
+
+        if (b > max) {
+            max = b;
+        }
+        if (c > max) {
+            max = c;
+        }
+
+        System.out.println("세 정수의 최대값은 :"+max);
+    }
+}
---
+++ Stack/src/com/yeGenieee/StackDynamicArray.java
@@ -0,0 +1,4 @@
+package com.yeGenieee;
+
+public class StackDynamicArray {
+}
