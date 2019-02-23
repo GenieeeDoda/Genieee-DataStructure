39955c9083617c30652067907b36bc3cf03ba071
yeGenieee <cyj95428@gmail.com>
2019-02-23 17:16:58 +0900

    ADD Ch1_2_Q10.java

---
+++ Iterations/src/main/java/ch01/Ch1_2_Q10.java
@@ -0,0 +1,26 @@
+package ch01;
+
+import java.util.Scanner;
+
+public class Ch1_2_Q10 {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("a의 값 : ");
+        int a = stdIn.nextInt();
+        int b;
+
+        System.out.print("b의 값 : ");
+        b = stdIn.nextInt();
+
+        while(b <=a) {
+            System.out.println("a 보다 큰 값을 입력하세요!");
+            System.out.print("b의 값 : ");
+            b = stdIn.nextInt();
+        }
+
+        System.out.println("b - a는 "+(b-a)+"입니다.");
+
+
+    }
+}
